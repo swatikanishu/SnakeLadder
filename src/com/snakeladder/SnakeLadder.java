2768ee14196f5887b95ab340e7bc100c8b29640a
swatikanishu <nishuswatika@gmail.com>
2022-06-19 20:24:05 +0530

    Added:Welcome message of snake ladder

---
+++ src/com/snakeladder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.snakeladder;
+
+public class SnakeLadder {
+        public static void main(String[] args) {
+            System.out.println("Welcome to snake and ladder");
+
+        }
+    }
+
