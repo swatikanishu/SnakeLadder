51d537589e8eca105326ae28c2130e991ab650e9
swatikanishu <nishuswatika@gmail.com>
2022-06-19 21:46:12 +0530

    Added:Functionality of dice roll

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -4,6 +4,9 @@ public class SnakeLadder {
         public static void main(String[] args) {
             System.out.println("Welcome to snake and ladder");
             int position =0;
+            int randomCheck=(int) (Math.random() * 10) % 6+1;
+            System.out.println(randomCheck);
+
         }
     }
 
