eaba874629ed2eb81cc340b64b2a4527d05815e0
swatikanishu <nishuswatika@gmail.com>
2022-06-19 20:31:36 +0530

    Added:position of snake ladder

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,7 +3,7 @@ package com.snakeladder;
 public class SnakeLadder {
         public static void main(String[] args) {
             System.out.println("Welcome to snake and ladder");
-
+            int position =0;
         }
     }
 
