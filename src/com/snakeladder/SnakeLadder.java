2e10dbd2b23b1849b15a62ebcde51f577381f3c4
swatikanishu <nishuswatika@gmail.com>
2022-06-19 21:59:59 +0530

    Added:Functionality modified

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -1,12 +1,27 @@
 package com.snakeladder;
 
 public class SnakeLadder {
-        public static void main(String[] args) {
-            System.out.println("Welcome to snake and ladder");
-            int position =0;
-            int randomCheck=(int) (Math.random() * 10) % 6+1;
-            System.out.println(randomCheck);
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder");
+        int position = 0;
+        int randomCheck = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println(randomCheck);
+        int randomCheck2 = (int) (Math.random() * 10) % 3;
+        switch (randomCheck2) {
+            case 0:
+                System.out.println("No play");
+                position += 0;
+                break;
+            case 1:
+                System.out.println("Ladder");
+                position += randomCheck;
+                break;
+            case 2:
+                System.out.println("Snake");
+                position -= randomCheck;
+                break;
 
         }
     }
+}
 
